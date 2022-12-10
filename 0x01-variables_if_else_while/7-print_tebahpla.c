8d9d6940d0ee996dc0ffcdc77b730a5dbc2776d6
andrewgilbert2018 <andrewgilbertandrewgilbert2018@gmail.com>
2022-12-10 13:29:23 -0800

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
