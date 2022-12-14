e967f1ca75383f41ceaac9eb2ed4d6a30455b25e
andrewgilbert2018 <andrewgilbertandrewgilbert2018@gmail.com>
2022-12-13 18:51:23 -0800

    5 sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,17 +11,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar(43);
 		return (1);
 	}
-	else if (n == 0)
+	else if (n < 0)
 	{
-		_putchar('0');
-		return (0);
+		_putchar(45);
+		return (-1);
 	}
 	else
 	{
-		putchar('-');
-		return (-1);
+		putchar(48);
+		return (0);
 	}
 }
