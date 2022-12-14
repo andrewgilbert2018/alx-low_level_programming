ba8cd84c221f4fa0c0e43301a1a7e47ea9ab8d4b
andrewgilbert2018 <andrewgilbertandrewgilbert2018@gmail.com>
2022-12-14 10:36:51 -0800

    100 times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,44 +8,43 @@
 
 void print_times_table(int n)
 {
+	int a = 0, rep, b;
 
-int a = 0, rep, b;
+	if (n < 0 || n > 15)
+		return;
 
-if (n < 0 || n > 15)
-	return;
-
-while (a <= n)
-{
-	for (b = 0; b <= n; b++)
+	while (a <= n)
 	{
-		rep = a * b;
-		if (b == 0)
-			_putchar('0' + rep);
-		else if (rep < 10)
+		for (b = 0; b <= n; b++)
 		{
-			_putchar(' ');
-			_putchar(' ');
-			_putchar('0' + rep);
+			rep = a * b;
+			if (b == 0)
+				_putchar('0' + rep);
+			else if (rep < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + rep);
+			}
+			else if (rep < 100)
+			{
+				_putchar(' ');
+				_putchar('0' + rep / 10);
+				_putchar('0' + rep % 10);
+			}
+			else
+			{
+				_putchar('0' + rep / 100);
+				_putchar(';' + (rep - 100) / 10);
+				_putchar('0' + rep % 10);
+			}
+			if (b < n)
+			{
+				_putchar(';');
+				_putchar(' ');
+			}
 		}
-		else if (rep < 100)
-		{
-			_putchar(' ');
-			_putchar('0' + rep / 10);
-			_putchar('0' + rep % 10);
-		}
-		else
-		{
-			_putchar('0' + rep / 100);
-			_putchar('0' + (rep - 100) / 10);
-			_putchar('0' + rep % 10);
-		}
-		if (b < n)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
+		_putchar('\n');
+		a++;
 	}
-	_putchar('\n');
-	a++;
-}
 }
