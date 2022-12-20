f8909a10d3dafad3012d8f90d48fa8e1deb92ecb
andrewgilbert2018 <andrewgilbertandrewgilbert2018@gmail.com>
2022-12-19 21:23:38 -0800

    101 keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -22,7 +22,6 @@ int main(void)
 		pass[i] = rand() % 78;
 		sum += (pass[i] + '0');
 		putchar(pass[i] + '0');
-
 		if ((2772 - sum) - '0' < 78)
 		{
 			n = 2772 - sum - '0';
@@ -31,4 +30,5 @@ int main(void)
 			break;
 		}
 	}
+	return (0);
 }
