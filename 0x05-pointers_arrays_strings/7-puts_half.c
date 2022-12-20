dc45cd063f8f76cf9c1bf4637efced2487f1d688
andrewgilbert2018 <andrewgilbertandrewgilbert2018@gmail.com>
2022-12-19 20:17:29 -0800

    8 print array

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,7 +10,7 @@ void puts_half(char *str)
 	int a, n, longi;
 
 	longi = 0;
-	for (a = 0; str[a] != '\0'; a++)
+	for (a = 0; str[a]; != '\0'; a++)
 		longi++;
 
 	n = (longi / 2);
@@ -18,7 +18,7 @@ void puts_half(char *str)
 	if ((longi % 2) == 1)
 		n = ((longi + 1) / 2);
 
-	for (a = n; str[a] !'\0'; a++)
+	for (a = n; str[a]; !'\0'; a++)
 		_putchar(str[a]);
 	_putchar('\n');
 }
