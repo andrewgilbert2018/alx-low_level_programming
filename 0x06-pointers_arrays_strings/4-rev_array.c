db6c4d1e7372d9e587edac5eb641682891eee533
andrewgilbert2018 <andrewgilbertandrewgilbert2018@gmail.com>
2022-12-22 19:53:27 -0800

    4 rev array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,12 +8,14 @@
 
 void reverse_array(int *a, int n)
 {
-	int tmp, index;
-	for (index = n - 1; index > n / 2; index--)
-		('\n')
+	int i;
+	int j;
+
+	for (i = 0; i < n; i++)
 	{
-		tmp = a[n - 1 - index];
-		a[n - 1 - index] = a[index];
-		a[index] = tmp;
+		n--;
+		j = a[i];
+		a[i] = a[n];
+		a[n] = j;
 	}
 }
