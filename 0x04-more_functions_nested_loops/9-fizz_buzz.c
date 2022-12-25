b03bd47ae8fc5ccda9824c8faac3cfbac9d1d39b
andrewgilbert2018 <andrewgilbertandrewgilbert2018@gmail.com>
2022-12-24 19:39:29 -0800

    9 fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,21 +4,26 @@
  *
  * Return: void
  */
+
 int main(void)
 {
+
 	int p = 100;
 	int i;
 
 	i = 1;
 	while (i <= p)
 	{
-		printf("FizzBuzz");
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+		printf("FizzBuzz ");
 	}
 
 	else if (i % 3 == 0)
 	{
 		printf("Fizz ");
 	}
+
 	else if (i % 5 == 0)
 	{
 		if (i < p)
