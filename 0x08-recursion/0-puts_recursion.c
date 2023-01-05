f10f99d69b539d38e4095a6adc37eba888a2957a
andrewgilbert2018 <andrewgilbertandrewgilbert2018@gmail.com>
2023-01-04 21:57:27 -0800

    0 puts recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+/*
+ * file 0-puts_recursion.c
+ * Auth: Andy
+ */
+
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string, followed by a new line.
+ * @s: the string to be printed.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n');
+}
+
