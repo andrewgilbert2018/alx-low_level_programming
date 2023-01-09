5b424b38bd818bebca4b5bf91e3c0be6c8c5192e
andrewgilbert2018 <andrewgilbertandrewgilbert2018@gmail.com>
2023-01-09 01:08:39 -0800

    3 mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -35,25 +35,26 @@ int _atoi(char *s)
 			f = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 
-					break;
-					f = 0;
-					}
-					i++;
-					}
+				break;
+			f = 0;
+		}
+		i++;
+	}
 
-					if (f == 0)
-					return (0);
-					return (n);
-					}
+	if (f == 0)
+		return (0);
 
-					/**
-					 * main - multiplies two numbers
-					 * @argc: number of arguments
-					 * @argv: array of arguments
-					 *
-					 * Return: 0 (Success), 1 (Error)
-					 */
-					int main(int argc, char *argv[])
+	return (n);
+}
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
+int main(int argc, char *argv[])
 {
 	int result, num1, num2;
 
