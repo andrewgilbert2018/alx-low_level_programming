ba73360ee653a35cb6587a3ec07960f20b12432a
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-03-21 06:58:15 -0700

    1 create dynamic library

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -Wall -Werror -pedantic -Wextra -std=gnu89 *.c -c -fPIC
+gcc -Wall -Werror -pedantic -Wextra *.c -c -fPIC
 gcc *.o -shared -o liball.so
-export LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$PWD.:$LD_LIBRARY_PATH
